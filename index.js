cc7777466f158b995cee61167c1093d6ad02e279
PakkuDon <pakku.pd@gmail.com>
2018-10-07 01:33:28 +1000

    Convert password length input to slider

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const result = document.querySelector('#result')
 const passwordLengthInput = document.querySelector('#password-length')
+const passwordLengthDisplay = document.querySelector('#password-length-display')
 const characterSetCheckboxes = Array.from(document.querySelectorAll('[name=character-sets]'))
+
+passwordLengthDisplay.textContent = passwordLengthInput.value
 
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault()
   const passwordLength = parseInt(passwordLengthInput.value, 10)
 
   result.value = PasswordGenerator.generate(passwordLength, getSelectedCharacterSets())
+})
+
+passwordLengthInput.addEventListener('input', (event) => {
+  passwordLengthDisplay.textContent = event.target.value
 })
 
 const getSelectedCharacterSets = () => (
