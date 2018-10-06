18135ee33b75703bf56ab43b113d7ad4818855fb
PakkuDon <pakku.pd@gmail.com>
2018-10-07 00:40:52 +1000

    Allow user to select charsets to generate password with

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const result = document.querySelector('#result')
 const passwordLengthInput = document.querySelector('#password-length')
+const characterSetCheckboxes = Array.from(document.querySelectorAll('[name=character-sets]'))
 
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault()
   const passwordLength = parseInt(passwordLengthInput.value, 10)
 
-  result.value = PasswordGenerator.generate(passwordLength, ['lower', 'upper', 'numbers', 'symbols'])
+  result.value = PasswordGenerator.generate(passwordLength, getSelectedCharacterSets())
 })
+
+const getSelectedCharacterSets = () => (
+  characterSetCheckboxes
+    .filter(checkbox => checkbox.checked)
+    .map(checkbox => checkbox.value)
+)
